591e62358b53ea8d376cf2d49302a9deca043e01
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-29 16:53:49 +0200

    function random category_id for each post

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -4,6 +4,8 @@ use App\Models\Post;
 use Illuminate\Database\Seeder;
 use Faker\Generator;
 use Illuminate\Support\Str;
+use App\Models\Category;
+use Illuminate\Support\Arr;
 
 class PostSeeder extends Seeder
 {
@@ -14,8 +16,12 @@ class PostSeeder extends Seeder
      */
     public function run(Generator $faker)
     {
+
+        $category_ids = Category::pluck('id')->toArray();
+
         for ($i = 0; $i < 5; $i++) {
             $post = new Post();
+            $post->category_id = Arr::random($category_ids);
             $post->title = $faker->text(25);
             $post->content = $faker->paragraph(2, true);
             $post->image = $faker->imageUrl(300, 300);
